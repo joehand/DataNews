bf4cee54192e1f713316ae42b8b3b220342fad5e
Joe Hand <joe@joehand.org>
2013-09-01 09:42:43 -0600

    Add basic error pages.

--- data_news/views.py
+++ data_news/views.py
@@ -208,3 +208,11 @@ def user(name):
 def about():
     return render_template('about.html')
 
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
