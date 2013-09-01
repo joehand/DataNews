68292f0f24ca548958a339463d303dab1f8e92af
Joe Hand <joe@joehand.org>
2013-08-31 23:10:20 -0600

    get downgrade script fixed too...

--- db_downgrade.py
+++ db_downgrade.py
@@ -1,7 +1,16 @@
 
 from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEMY_MIGRATE_REPO
+
+if os.environ.get('HEROKU_PROD', False):
+    from config import ProductionConfig
+    config = ProductionConfig
+else:
+    from config import DevelopmentConfig
+    config = DevelopmentConfig
+
+SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
+SQLALCHEMY_MIGRATE_REPO = config.SQLALCHEMY_MIGRATE_REPO
+
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
 print 'Current database version: ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
