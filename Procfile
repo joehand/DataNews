7de5dd33b188907bd1ef54d12ecf1705c2328412
Joe Hand <joe@joehand.org>
2013-09-01 19:26:40 -0600

    Add db migration scripts

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn data_news:app -b "0.0.0.0:$PORT" -w 9 -k gevent
-upgrade_db: alembic upgrade head
 fetch_twitter: python manage.py twitter
+db_upgrade: python manage.py db_upgrade
+db_migrate: python manage.py db_migrate
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 # manage.py
 from data_news import app
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from tweets import TweetGetter
 import os
 
@@ -8,15 +8,24 @@ manager = Manager(app)
 tweets = TweetGetter()
 
 @manager.command
-def hello():
-    print "hello"
-
-@manager.command
 def twitter():
     tweets.get_mentions();
     tweets.get_favorites();
     print 'all done!'
 
+@manager.command
+def db_upgrade():
+    os.system('alembic upgrade head')
+
+@manager.option('-m', '--message', dest='message', default='')
+def db_migrate(message):
+    print message
+    os.system('alembic revision --autogenerate -m "%s"' % message)
+
+def shell_context():
+    return dict(app=app)          
+
 #runs the app
 if __name__ == '__main__':
+    manager.add_command('shell', Shell(make_context=shell_context))
     manager.run()
